de404ead220289d09147a41d1ae81537e177f4b1
katya <abramushkinakatya@gmail.com>
2022-10-02 21:08:33 +0700

    Add the calculation of Princess's happiness points

--- src/Castle.cs
+++ src/Castle.cs
@@ -20,15 +20,49 @@ public class Castle
     {
         Hall.FillContendersList();
 
-        Contender? prince = Princess.ChoosePrince(Hall.ContenderList);
+        var chosenPrince = Princess.ChoosePrince(Hall.ContenderList);
+        
+        PrintResult(chosenPrince);
+    }
 
-        // TODO: class FileWriter
+    /// <summary>
+    /// Prints the list of reviewed contenders and the Princess's happiness
+    /// points. Princess gets:
+    /// a) 10 points if she does not choose any of the contenders,
+    /// b) 0 if the score of the chosen contender is less than 51,
+    /// c) points are equal to the contender's score (from 51 to 100) otherwise.
+    /// </summary>
+    /// <param name="chosenPrince"> The prince who was chosen </param>
+    private void PrintResult(Contender? chosenPrince)
+    {
         using var writer = new StreamWriter(Constants.ResultPath, false);
         foreach (var contender in Princess.LadyInWaiting.ContenderList)
         {
             writer.WriteLine($"{contender.Score} {contender.Name}");
         }
-        writer.WriteLine("=============");
-        writer.WriteLine(prince == null ? "Did not choose a prince!" : $"{prince.Score} {prince.Name}");
+        writer.WriteLine("===");
+
+        var princessPoints = CalculatePrincessPoints(chosenPrince);
+        switch (princessPoints)
+        {
+            case 0:
+                writer.WriteLine($"Oh, bad choice! Happiness points: {princessPoints}");
+                break;
+            case 10:
+                writer.WriteLine($"Did not choose a prince! Happiness points: {princessPoints}");
+                break;
+            default:
+                writer.WriteLine($"...and they lived happily ever after! Happiness points: {princessPoints}");
+                break;
+        }
+    }
+
+    private static int CalculatePrincessPoints(Contender? chosenPrince)
+    {
+        if (chosenPrince == null)
+        {
+            return 10;
+        }
+        return chosenPrince.Score > 50 ? chosenPrince.Score : 0 ;
     }
 }
