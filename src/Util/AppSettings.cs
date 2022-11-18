cc5444689645fcec3267e101d9c92152746741e4
Ekaterina <abramushkinakatya@gmail.com>
2022-11-18 12:46:04 +0700

    Revert "Move project to a separate file, create project for tests"
    This reverts commit b6a6c4dd48c375e3a5afbb502b41095a8786a8ee.

--- src/Util/AppSettings.cs
+++ src/Util/AppSettings.cs
@@ -7,17 +7,32 @@ public static class AppSettings
     /// <summary>
     /// Total number of contenders 
     /// </summary>
-    public static int ContenderCount { get; private set; } = 100;
+    private const int DefaultContenderCount = 100;
 
     /// <summary>
     /// Path to the file with names for contenders, must contain 100 unique names
     /// </summary>
-    public static string ContenderPath { get; private set; } = "../../../res/100-unique-names.txt";
-
+    private const string DefaultContenderPath = "../../../res/100-unique-names.txt";
+    
     /// <summary>
     /// Path to the file with the printed algorithm's result
     /// </summary>
-    public static string ResultPath { get; private set; } = "../../../res/result.txt";
+    private const string DefaultResultPath = "../../../res/result.txt";
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public static int ContenderCount { get; private set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static string ContenderPath { get; private set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public static string ResultPath { get; private set; }
 
     public static void LoadConfigurationSettings(IConfiguration configuration)
     {
@@ -28,25 +43,22 @@ public static class AppSettings
 
     private static void SetContenderCount(string? value)
     {
-        if (!string.IsNullOrEmpty(value))
-        {
-            ContenderCount = int.Parse(value);
-        }
+        ContenderCount = string.IsNullOrEmpty(value) 
+            ? DefaultContenderCount 
+            : int.Parse(value);
     }
     
     private static void SetContenderPath(string? value)
     {
-        if (!string.IsNullOrEmpty(value))
-        {
-            ContenderPath = value;
-        }
+        ContenderPath = string.IsNullOrEmpty(value) 
+            ? DefaultContenderPath 
+            : value;
     }
     
     private static void SetResultPath(string? value)
     {
-        if (!string.IsNullOrEmpty(value))
-        {
-            ResultPath = value;
-        }
+        ResultPath = string.IsNullOrEmpty(value) 
+            ? DefaultResultPath 
+            : value;
     }
 }
