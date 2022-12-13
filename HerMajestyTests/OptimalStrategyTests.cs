e54003a372b20e376fde93d5a901dbcbff00dab6
Ekaterina <abramushkinakatya@gmail.com>
2022-12-14 00:19:38 +0700

    Add tests for OptimalStrategy

--- HerMajestyTests/OptimalStrategyTests.cs
+++ HerMajestyTests/OptimalStrategyTests.cs
@@ -1,10 +1,68 @@
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
+using HerMajesty.Model;
+using HerMajesty.Strategy;
+using HerMajesty.Util;
+using HerMajestyTests.Mock;
+
 namespace HerMajestyTests;
 
 [TestFixture]
 public class OptimalStrategyTests
 {
-    [Test]
-    public void Method_Conditions_Returns()
+    private const int MaxContenderScore = AppSettings.DefaultContenderCount;
+    private const int CutOffContenderScore = 38; // Equals to {DefaultContenderCount}/ Math.E
+
+    private IStrategy _strategy;
+    private IHall _hall;
+
+    private readonly IContenderListGenerator _mockedGenerator = Substitute.For<IContenderListGenerator>();
+    
+    [SetUp]
+    public void SetupStrategy()
     {
+        _hall = new Hall(_mockedGenerator);
+        _strategy = new OptimalStrategy(
+            new Friend(), 
+            _hall, 
+            Substitute.For<ILogger<OptimalStrategy>>());
+    }
+    
+    [Test]
+    public void ChooseBestContender_ScoreOfChosenContenderGreaterThan50_ReturnsContender()
+    {
+        _mockedGenerator.GenerateContenderList().Returns(
+            MockContenderListGenerator.GenerateMaximalHappinessList()
+            );
+        _hall.FillContendersList();
+
+        var chosen = _strategy.ChooseBestContender();
+        chosen.Should().NotBeNull();
+        chosen?.Score.Should().Be(MaxContenderScore);
+    }
+    
+    [Test]
+    public void ChooseBestContender_ScoreOfChosenContenderLessThan50_ReturnsContender()
+    {
+        _mockedGenerator.GenerateContenderList().Returns(
+            MockContenderListGenerator.GenerateAscendingList()
+            );
+        _hall.FillContendersList();
+    
+        var chosen = _strategy.ChooseBestContender();
+        chosen.Should().NotBeNull();
+        chosen?.Score.Should().Be(CutOffContenderScore);
+    }
+    
+    [Test]
+    public void ChooseBestContender_NoContenderChosen_ReturnsNull()
+    {
+        _mockedGenerator.GenerateContenderList().Returns(
+            MockContenderListGenerator.GenerateDescendingList()
+            );
+        _hall.FillContendersList();
+        _strategy.ChooseBestContender().Should().BeNull();
     }
 }
--- HerMajestyTests/PrincessTests.cs
+++ HerMajestyTests/PrincessTests.cs
@@ -1,64 +1,35 @@
 using FluentAssertions;
-using Microsoft.Extensions.Logging;
-using NSubstitute;
-
 using HerMajesty.Model;
-using HerMajesty.Strategy;
-using HerMajestyTests.Mock;
 
 namespace HerMajestyTests;
 
 [TestFixture]
 public class PrincessTests
 {
-    /*
-     * Naming rules for tests:
-     * 
-     * [Test]
-     * public void Method_Conditions_Returns() { }
-     */
-    
-    // private const int ContendersCount = 100;
-    
-    private IHall _hall;
-    private Princess _princess;
-    private IContenderListGenerator _generator;
+    /// Note: Princess's method `ChoosePrince()` does not have any tests since it completely matches
+    /// the `ChooseBestContender()` method in used IStrategy implementation
 
-    [SetUp]
-    public void Setup()
+    private const int ScoreGreaterThanHappinessBoundary = 100;   
+    private const int ScoreLessThanHappinessBoundary    = 10;
+
+    [Test]
+    public void CalculateHappinessPoints_ScoreGreaterThanHappinessBoundary_ReturnsPrinceScore()
     {
-        _generator = Substitute.For<IContenderListGenerator>();
-        _hall = new Hall(_generator);
-        
-        IStrategy strategy = new OptimalStrategy(new Friend(), _hall, Substitute.For<ILogger<OptimalStrategy>>());
-        _princess = new Princess(strategy);
+        Princess.CalculateHappinessPoints(ScoreGreaterThanHappinessBoundary)
+            .Should().Be(ScoreGreaterThanHappinessBoundary);
+    }
+    
+    [Test]
+    public void CalculateHappinessPoints_ScoreLessThanHappinessBoundary_ReturnsBadPrinceChosenScore()
+    {
+        Princess.CalculateHappinessPoints(ScoreLessThanHappinessBoundary)
+            .Should().Be(Princess.BadPrinceChosenScore);
     }
 
     [Test]
-    public void ChoosePrince_ScoreOfChosenContenderGreaterThan50_ReturnsContender()
+    public void CalculateHappinessPoints_NoPrinceChosen_ReturnsNoPrinceChosenScore()
     {
-        _generator.GenerateContenderList().Returns(MockContenderListGenerator.GenerateMaximalHappinessList());
-        _hall.FillContendersList();
-        var chosen = _princess.ChoosePrince();
-        chosen.Should().NotBeNull();
-        chosen?.Score.Should().Be(100);
-    }
-    
-    [Test]
-    public void ChoosePrince_ScoreOfChosenContenderLessThan50_ReturnsContender()
-    {
-        _generator.GenerateContenderList().Returns(MockContenderListGenerator.GenerateAscendingList());
-        _hall.FillContendersList();
-        var chosen = _princess.ChoosePrince();
-        chosen.Should().NotBeNull();
-        chosen?.Score.Should().Be(37);
-    }
-    
-    [Test]
-    public void ChoosePrince_NoContenderChosen_ReturnsNull()
-    {
-        _generator.GenerateContenderList().Returns(MockContenderListGenerator.GenerateDescendingList());
-        _hall.FillContendersList();
-        _princess.ChoosePrince().Should().BeNull();
+        Princess.CalculateHappinessPoints(null)
+            .Should().Be(Princess.NoPrinceChosenScore);
     }
 }
