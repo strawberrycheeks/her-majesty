de4be010e07a6a66c63a61b14a3c71087d837c7b
Ekaterina <abramushkinakatya@gmail.com>
2022-12-20 15:59:29 +0700

    Test database connection

--- HerMajestyDatabase/Program.cs
+++ HerMajestyDatabase/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using HerMajestyDatabase;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    User user1 = new User { Name = "Tom", Age = 33 };
+    User user2 = new User { Name = "Alice", Age = 26 };
+    
+    db.Users.AddRange(user1, user2);
+    db.SaveChanges();
+}
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    var users = db.Users.ToList();
+    Console.WriteLine("Users list:");
+    foreach (User u in users)
+    {
+        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+    }
+}
