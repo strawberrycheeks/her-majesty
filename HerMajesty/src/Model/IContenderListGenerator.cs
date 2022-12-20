a633d74d652916e7ed0257877681252abcda0ebf
Ekaterina <abramushkinakatya@gmail.com>
2022-12-20 14:56:37 +0700

    Remove resource file for tests

--- HerMajesty/src/Model/IContenderListGenerator.cs
+++ HerMajesty/src/Model/IContenderListGenerator.cs
@@ -6,4 +6,10 @@ public interface IContenderListGenerator
     /// Returns the filled and shuffled list of contenders
     /// </summary>
     public List<Contender> GenerateContenderList();
+    
+    /// <summary>
+    /// Reads a list of name from a text file {filepath} and creates
+    /// a shuffled enumerated list of contenders
+    /// </summary>
+    public List<Contender> GenerateContenderList(string filepath);
 }
--- HerMajestyTests/ContenderListGeneratorTests.cs
+++ HerMajestyTests/ContenderListGeneratorTests.cs
@@ -7,6 +7,7 @@ namespace HerMajestyTests;
 [TestFixture]
 public class ContenderListGeneratorTests
 {
+    private readonly string _filepath = "../../../../HerMajesty/res/100-unique-names.txt";
     private IContenderListGenerator _generator;
     
     [SetUp]
@@ -18,7 +19,7 @@ public class ContenderListGeneratorTests
     [Test]
     public void GenerateContenderList_ReturnsListOf100UniqueContenders()
     {
-        var generatedList = _generator.GenerateContenderList();
+        var generatedList = _generator.GenerateContenderList(_filepath);
         generatedList.Count.Should().Be(100);
         generatedList.Should().OnlyHaveUniqueItems();
     }
